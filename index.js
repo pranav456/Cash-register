08b328f0dc17c744ef4f7eddb3770673441fbc26
pranav456 <47733546+pranav456@users.noreply.github.com>
2022-09-03 00:39:29 +0530

    logic

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-var billAmount = document.querySelector("#bill-amount");
-var cashGiven = document.querySelector("#cash-given");
-var checkButton = document.querySelector("#check-button");
-var message = document.querySelector("#error-message");
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
 
-checkButton.addEventListener("click", function validateBillAmount() {
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+
+checkButton.addEventListener("click", function validateBillAndCashAmount() {
   hideMessage();
+  var cash = Number(cashGiven.value);
+  var bill = Number(billAmount.value);
 
-  if (billAmount.value > 0) {
-    if (cashGiven.value >= billAmount.value) {
-      const amountToBeReturned = cashGiven.value - billAmount.value;
+  if (bill > 0) {
+    if (cash > bill) {
+      const amountToBeReturned = cash - bill;
       calculateChange(amountToBeReturned);
     } else {
       showMessage("The cash provided should atleast be equal to bill amount");
@@ -17,6 +22,14 @@ checkButton.addEventListener("click", function validateBillAmount() {
     showMessage("Invalid bill amount");
   }
 });
+
+function calculateChange(amountToBeReturned) {
+  for (let i = 0; i < availableNotes.length; i++) {
+    const numberofnotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+    amountToBeReturned = amountToBeReturned % availableNotes[i];
+    noOfNotes[i].innerText = numberofnotes;
+  }
+}
 
 function hideMessage() {
   message.style.display = "none";
